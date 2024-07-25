ec7b403ebf17c7081430374f9fc9dbf16e277dff
RexX <1-bubi@web.de>
2024-07-25 17:06:19 +0300

    Add files via upload

---
+++ Credits.m
@@ -0,0 +1,12 @@
+//written in Objectiv**** by Rex
+
+
+//Credits-Big thanks to
+
+@Prosursus         -roothide-
+@ixi07             -side development-
+@opa334            -trollstore helper-
+@roothidedev       -Bootstrap injection-
+@Iscshd            -main development-
+
+//You can find all these people on GitHub!
